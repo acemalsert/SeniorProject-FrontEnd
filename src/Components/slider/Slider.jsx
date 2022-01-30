0919d7f2361e63dc3d9823cfe81a67a25cb4d250
Sina Postacı <sina.postaci.tedu@gmail.com>
2022-01-30 20:08:40 +0300

    slider update

--- src/Components/slider/Slider.jsx
+++ src/Components/slider/Slider.jsx
@@ -25,12 +25,16 @@ function Slider() {
 
         const newSlide = document.querySelector("#slide"+next);
         const newslideImg = newSlide.querySelector("img");
+        const newHeading = newSlide.querySelector("div");
 
-        const active =  slider.querySelector("[data-active]");
-        if(newslideImg === active) return;
+        const active =  slider.querySelectorAll("[data-active]");
+        if(newslideImg === active[0]) return;
 
-        delete active.dataset.active;
+        delete active[0].dataset.active;
+        delete active[1].dataset.active;
+        
         newslideImg.dataset.active = true;
+        newHeading.dataset.active=true;
 
     },[next]);
     return(
@@ -41,12 +45,24 @@ function Slider() {
             </div>
             <div id = "slide1">
                 <img className = "slider-img" data-active src="https://i.redd.it/7ovvnp2ccyx51.png" alt="" />
+                <div className = "slide-heading" data-active>
+                    <h2 >Predator Medya</h2>
+                    <p>Oyuncuların bir numaralı adresi!</p>
+                </div>
             </div>
             <div id = "slide2">
                 <img className = "slider-img" src="https://1.bp.blogspot.com/-rG0OpZsIA-c/Xu99ATAhUZI/AAAAAAAADbk/amIOJQ4GCEwLf6AmP4SQv8bjx2YS_2QGgCK4BGAsYHg/s3840/wallapaper%2Bvalorant%2B01.jpg" alt="" />
+                <div className = "slide-heading">
+                    <h2 >Oyun ile alakalı her şey!</h2>
+                    <p>Oyun sektorüyle alakalı son dakika haberleri,oyuncu forumları ve daha niceleri!</p>
+                </div>
             </div>
             <div id = "slide3">
                 <img className = "slider-img" src="https://3.bp.blogspot.com/-LJH71ajOVow/XPrt54OKCfI/AAAAAAAAG1E/EdgNW13s1LoNqAeJwelgblTW41Uf1f1lQCKgBGAs/w0/dying-light-2-uhdpaper.com-4K-7.jpg" alt="" />
+                <div className = "slide-heading">
+                    <h2 >İletişim kur!</h2>
+                    <p>Üye olduktan sonra arkadaşlarınla rahatlıkla mesajlaşabilir,forumdan fikir belirtebilirsin!</p>
+                </div>
             </div>
         </div>
     );
