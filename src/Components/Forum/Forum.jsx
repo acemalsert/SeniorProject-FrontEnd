ec434740a4209fcc182066057706d6e5e65de3d5
talip-git <sina.postaci.tedu@gmail.com>
2022-03-14 13:13:18 +0300

    forum update

--- src/Components/Forum/Forum.jsx
+++ src/Components/Forum/Forum.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import forumData from '../../DummyData/ForumData';
 import './forum.css';
+import axios from 'axios';
+
+ function DisplayComments({forumId}) {
+    const [commentsLength,setCommentsLength] = useState("")
+    useEffect(()=>{
+        const fetchComment = async(forumId)=>{
+            try {
+                const res = await axios.get(`/comments/forum/${forumId}`)
+                if(res.data.length <= 0){
+                    setCommentsLength("Henüz yorum yok")
+                    return
+                }
+                setCommentsLength(String(res.data.length)+" yorum")
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchComment(forumId)
+    },[])
+    return (
+        <div><p>{commentsLength}</p></div>
+    );
+}
+
 function Forum() {
-  return (
+    const [forums,setForums] = useState([])
+    useEffect(()=>{
+        const fetchForumEntries = async ()=>{
+            try {
+                const res = await axios.get('/forum/getForum')
+                setForums(res.data);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchForumEntries()
+    },[])
+    return (
     <div className='container'>
         <div className='row'>
-            {forumData.map((entry)=>{
+            {forums.map((entry)=>{
                 return(
-                    <div className='col-12 col-md-4' key={entry.forumId}>
+                    <div className='col-12 col-md-4' key={entry._id}>
                         <div className='forum-card'>
                             <div className='forumCard-heading'>
                                 <small><i className="fa-solid fa-user"></i></small>
@@ -19,7 +55,7 @@ function Forum() {
                             </div>
                             <div className='forumCard-footer'>
                                 <hr />
-                                <p>{entry.comments.length} Yorum</p>
+                                <DisplayComments forumId={entry._id}/>
                             </div>
                         </div>
                     </div>
@@ -27,7 +63,7 @@ function Forum() {
             })}
         </div>
     </div>
-  );
+    );
 }
 
 export default Forum
