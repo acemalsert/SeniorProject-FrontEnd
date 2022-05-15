6b3d0348fbd6c8bdb66e5de98d565b7ae5f28754
Ahmet Cemal Sert <acemalsert@gmail.com>
2022-05-15 17:11:33 +0300

    .

--- src/Components/InduvidualNews/InduvidualNews.jsx
+++ src/Components/InduvidualNews/InduvidualNews.jsx
@@ -33,7 +33,7 @@ function InduvidualNews() {
   
   const fetchNewsComments = async ()=>{
     try {
-      const res = await axios.get(`http://localhost:5000/api/comments/news/${newsId}/${induvidualNews.userId}`)
+      const res = await axios.get(`http://localhost:5000/api/comments/news/${newsId}/${user._id}`)
       setComments(res.data)
     } catch (error) {
       console.log(error)
